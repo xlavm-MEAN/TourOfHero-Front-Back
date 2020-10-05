58dc70509fc2a63f8b99215a56af7ddcf8a20ccb
Luis Angel Vanegas <luanvane@bancolombia.com.co>
2020-10-05 10:50:21 -0500

    final functional

--- Front-Angular/src/main.ts
+++ Front-Angular/src/main.ts
@@ -1,4 +1,3 @@
-
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
@@ -9,7 +8,4 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
-
-
+platformBrowserDynamic().bootstrapModule(AppModule);
